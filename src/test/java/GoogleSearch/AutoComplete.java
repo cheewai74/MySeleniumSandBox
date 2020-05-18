c4b57a3fe13913c5cd89526368835bfc09823172
MosesWong <academicsnotes@gmail.com>
2020-05-18 13:15:41 +0800

    Google search Sandbox - impicit and explicit wait

--- src/test/java/GoogleSearch/AutoComplete.java
+++ src/test/java/GoogleSearch/AutoComplete.java
@@ -1,38 +1,36 @@
 package GoogleSearch;
 
-import okio.Timeout;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.sleep;
 
-public class KeyboardAndMouseInput {
+public class AutoComplete {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args)  {
 
         System.setProperty( "webdriver.chrome.driver", "resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://formy-project.herokuapp.com");
 
         // Explicit Wait
-       // WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
 
-        WebElement clickLink = driver.findElement(By.linkText("Autocomplete"));
-        clickLink.click();
-        sleep(1000);
+        driver.get("https://formy-project.herokuapp.com/autocomplete");
+
         WebElement autocomplete = driver.findElement(By.id("autocomplete"));
         autocomplete.sendKeys("Clementi Avenue 1, Singapore");
-       // sleep(1000);
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // implicit Wait
+        // sleep(1000);
 
-        WebElement autocompleteResults = driver.findElement(By.className("pac-item"));
-        //sleep(1000);
-       // driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // -- Implicit Wait
+       // WebElement autocompleteResults = driver.findElement(By.className("pac-item"));
+        WebElement autocompleteResults = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("pac-item")));
         autocompleteResults.click();
         driver.quit();
     }
