869953990103b2a3337ae09331308a786285ec9a
MosesWong <academicsnotes@gmail.com>
2020-05-18 11:19:30 +0800

    Google search Sandbox - Dropdown

---
+++ src/test/java/GoogleSearch/Dropdown.java
@@ -0,0 +1,28 @@
+package GoogleSearch;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Dropdown {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/dropdown");
+
+        WebElement dropDownMenu = driver.findElement(By.id("dropdownMenuButton"));
+        dropDownMenu.click();
+
+        WebElement autocompleteItems = driver.findElement(By.id("autocomplete"));
+        autocompleteItems.click();
+
+        driver.quit();
+    }
+
+
+}
